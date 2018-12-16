684da4a550dd2270db02cc17273af71904fe75ac
Ingo Bürk <ingo.buerk@tngtech.com>
2018-12-16 16:04:49 +0100

    chore(docs): Update ngqp-demo styling a bit

--- projects/ngqp-demo/src/app/app.component.ts
+++ projects/ngqp-demo/src/app/app.component.ts
@@ -3,6 +3,13 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
+    styles: [`
+        .logo-container {
+            background-color: white;
+            border-radius: 50%;
+            margin-right: 8px;
+        }
+    `],
 })
 export class AppComponent {
 
