cc7334f52f6282b16c0454a09881fca8ba13757f
Ingo Bürk <ingo.buerk@tngtech.com>
2018-12-16 14:21:31 +0100

    fix(core): Export serializers

--- projects/ngqp/core/src/lib/core.ts
+++ projects/ngqp/core/src/lib/core.ts
@@ -4,4 +4,19 @@ export { QueryParamGroupDirective }  from './query-param-group.directive';
 export { QueryParamBuilder } from './query-param-builder.service';
 export { QueryParamModule } from './query-param.module';
 
+export {
+    createStringSerializer,
+    createStringDeserializer,
+    createNumberSerializer,
+    createNumberDeserializer,
+    createBooleanSerializer,
+    createBooleanDeserializer,
+    DEFAULT_STRING_SERIALIZER,
+    DEFAULT_STRING_DESERIALIZER,
+    DEFAULT_NUMBER_SERIALIZER,
+    DEFAULT_NUMBER_DESERIALIZER,
+    DEFAULT_BOOLEAN_SERIALIZER,
+    DEFAULT_BOOLEAN_DESERIALIZER,
+} from './serializers';
+
 export * from './accessors/accessors';
