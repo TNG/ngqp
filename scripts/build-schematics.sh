1cd8dfab6f36fbf00acfee3a328cfdbf8d267820
Ingo Bürk <ingo.buerk@tngtech.com>
2018-12-25 23:52:50 +0100

    fix(other): Fix schematics build script

--- scripts/build-schematics.sh
+++ scripts/build-schematics.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 tsc -p projects/ngqp/core/tsconfig.schematics.json
-cp projects/ngqp/core/schematics/collection.json dist/ngqp/core
+cp projects/ngqp/core/collection.json dist/ngqp/core
