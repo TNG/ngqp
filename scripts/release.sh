91ad58ae6789a8f8c5ca1fda6d443d64ef487a7e
Ingo Bürk <ingo.buerk@tngtech.com>
2018-12-26 00:01:10 +0100

    chore(other): Added TODOs

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# TODO: Copy README
 
 yarn run lint || exit 1
 
@@ -7,4 +8,5 @@ yarn run changelog:build || exit 1
 yarn run core:build || exit 1
 yarn run schematics:build || exit 1
 
+# TODO Why does this not work?
 cd dist/ngqp/core && npm publish
