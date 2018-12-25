4865d5f664cdc05e5c641af5cee25491f43c81fe
Ingo Bürk <ingo.buerk@tngtech.com>
2018-12-26 00:51:55 +0100

    fix(other): Fail on errors when building schematics

--- scripts/build-schematics.sh
+++ scripts/build-schematics.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# If any command fails, stop immediately
+set -e -o pipefail
+
 tsc -p projects/ngqp/core/tsconfig.schematics.json
 cp projects/ngqp/core/collection.json dist/ngqp/core
