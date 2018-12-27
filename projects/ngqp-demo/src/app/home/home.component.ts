79a340d18a1a0050626baeffbbd58d663340237d
Ingo Bürk <ingo.buerk@tngtech.com>
2018-12-27 05:06:55 +0100

    fix(docs): Update wording

--- projects/ngqp-demo/src/app/home/home.component.ts
+++ projects/ngqp-demo/src/app/home/home.component.ts
@@ -19,7 +19,7 @@ export class HomeComponent {
             icon: faGlassCheers,
             title: 'Simple',
             description: `
-                We know that you expect things to just work. That's why ngqp requires almost no setup and is easy to use while
+                Developers expect tools to just work. That's why ngqp requires almost no setup and is easy to use while
                 giving you the flexibility to configure it to your needs. The API feels familiar because it is designed after
                 the Forms API.
             `,
@@ -28,16 +28,16 @@ export class HomeComponent {
             icon: faCogs,
             title: 'Powerful',
             description: `
-                Dependencies should earn their keep; that's why ngqp ships with a lot of features packed into an intuitive API
-                that allow you to do exactly what you need to do.
+                Any tool is only as good as it is useful; that's why ngqp ships with a lot of features that allow you to do
+                exactly what you need to do, nicely packed into an intuitive API so you don't need to scratch your head.
             `,
         },
         {
             icon: faAlignLeft,
             title: 'Documented',
             description: `
-                No one should have a hard time learning how to use something. We provide rich documentation with many examples,
-                guides and tips & tricks. If you have any issue getting something to work, the community will be right by your
+                Reading documentation doesn't have to be a bother. We provide rich documentation with many examples,
+                guides and tips & tricks. If you have issues getting something to work, the community will be right by your
                 side and help you out.
             `,
         },
@@ -45,9 +45,8 @@ export class HomeComponent {
             icon: faHeart,
             title: 'Free',
             description: `
-                We ❤️ the open-source community, so there are no hidden fees or strings attached. ngqp is free, open-source and
-                MIT-licensed. Find the source code on Github, fork it, or help improving it by contributing documentation,
-                bug fixes or features.
+                We ❤️ the open source community, so there are no strings attached. ngqp is free, open source and MIT-licensed.
+                Find the source code on Github, fork it, or help improving it by contributing documentation, bug fixes or features.
             `,
         },
     ];
