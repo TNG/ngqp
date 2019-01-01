31581d684de1944bb8c703ac1e90e3de07a22894
Ingo Bürk <ingo.buerk@tngtech.com>
2019-01-01 23:12:03 +0100

    chore(docs): List methods first

--- projects/ngqp-demo/src/app/shared/docs-link/docs-link.component.ts
+++ projects/ngqp-demo/src/app/shared/docs-link/docs-link.component.ts
@@ -1,13 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Host, Input, OnInit, Optional } from '@angular/core';
 import { DocsPage } from '../../docs-page';
+import { DocsItemComponent } from '../docs-item/docs-item.component';
 
 @Component({
     selector: 'docs-link',
     templateUrl: './docs-link.component.html',
 })
-export class DocsLinkComponent {
+export class DocsLinkComponent implements OnInit {
 
     @Input()
     public page: DocsPage;
+
+    @Input()
+    public fragment: string;
+
+    constructor(@Host() @Optional() private docsItem: DocsItemComponent) {}
+
+    public ngOnInit(): void {
+        if (this.page || !this.docsItem) {
+            return;
+        }
+
+        this.page = this.docsItem.docsPage;
+    }
 
 }
