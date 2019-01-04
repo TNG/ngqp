ca45e43ab5de167bcb2ad6902256e82f747351e2
Ingo Bürk <ingo.buerk@tngtech.com>
2019-01-04 19:21:27 +0100

    fix(core): Export createEmptyOnDeserializer

--- projects/ngqp/core/src/lib/core.ts
+++ projects/ngqp/core/src/lib/core.ts
@@ -7,6 +7,7 @@ export { ControlValueAccessorDirective } from './control-value-accessor.directiv
 
 export {
     createEmptyOnSerializer,
+    createEmptyOnDeserializer,
     createStringSerializer,
     createStringDeserializer,
     createNumberSerializer,
