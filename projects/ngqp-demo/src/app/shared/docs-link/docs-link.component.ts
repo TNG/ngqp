a0bd7285a65390dbe491532709751471152993ac
Ingo Bürk <ingo.buerk@tngtech.com>
2019-01-04 20:23:13 +0100

    chore(docs): Allow custom link content for docs link

--- projects/ngqp-demo/src/app/shared/docs-link/docs-link.component.ts
+++ projects/ngqp-demo/src/app/shared/docs-link/docs-link.component.ts
@@ -1,4 +1,4 @@
-import { Component, Host, Input, OnInit, Optional } from '@angular/core';
+import { AfterContentInit, Component, ElementRef, Host, Input, OnInit, Optional, ViewChild } from '@angular/core';
 import { DocsPage } from '../../docs-page';
 import { DocsItemComponent } from '../docs-item/docs-item.component';
 
@@ -6,13 +6,18 @@ import { DocsItemComponent } from '../docs-item/docs-item.component';
     selector: 'docs-link',
     templateUrl: './docs-link.component.html',
 })
-export class DocsLinkComponent implements OnInit {
+export class DocsLinkComponent implements OnInit, AfterContentInit{
+
+    @ViewChild('content', { read: ElementRef })
+    private contentNode: ElementRef<HTMLDivElement>;
 
     @Input()
     public page: DocsPage;
 
     @Input()
     public fragment: string;
+
+    public customText: string;
 
     constructor(@Host() @Optional() private docsItem: DocsItemComponent) {}
 
@@ -22,6 +27,15 @@ export class DocsLinkComponent implements OnInit {
         }
 
         this.page = this.docsItem.docsPage;
+    }
+
+    public ngAfterContentInit(): void {
+        if (!this.contentNode) {
+            return;
+        }
+
+        const content = this.contentNode.nativeElement.innerText.trim();
+        this.customText = content.length !== 0 ? content : null;
     }
 
 }
