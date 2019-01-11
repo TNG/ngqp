bbc9e02b9b9f21fe019faf7c28fab9fe46377cae
Ingo Bürk <ingo.buerk@tngtech.com>
2019-01-11 14:11:17 +0100

    chore(tests): Added more tests for QueryParam

--- projects/ngqp/core/src/lib/model/query-param.spec.ts
+++ projects/ngqp/core/src/lib/model/query-param.spec.ts
@@ -1,20 +1,23 @@
 import { QueryParam } from './query-param';
 import { QueryParamOpts } from './query-param-opts';
 import { LOOSE_IDENTITY_COMPARATOR } from '../util';
-
-const opts: Required<QueryParamOpts<any>> = {
-    param: 'q',
-    serialize: (value: any) => '',
-    deserialize: (value: string) => value,
-    multi: false,
-    debounceTime: undefined,
-    emptyOn: undefined,
-    compareWith: undefined,
-    combineWith: undefined,
-};
+import { DEFAULT_STRING_DESERIALIZER, DEFAULT_STRING_SERIALIZER } from '../serializers';
+import { fakeAsync } from '@angular/core/testing';
+import { take } from 'rxjs/operators';
 
 describe(QueryParam.name, () => {
     describe('constructor', () => {
+        const opts: Required<QueryParamOpts<any>> = {
+            param: 'q',
+            serialize: (value: any) => '',
+            deserialize: (value: string) => value,
+            multi: false,
+            debounceTime: undefined,
+            emptyOn: undefined,
+            compareWith: undefined,
+            combineWith: undefined,
+        };
+
         it('throws an error if no parameter is provided', () => {
             expect(() => new QueryParam({
                 ...opts,
@@ -73,5 +76,94 @@ describe(QueryParam.name, () => {
                 combineWith: 42 as any,
             })).toThrowError('combineWith must be a function, but received 42');
         });
+    });
+
+    describe('setValue', () => {
+        let queryParam: QueryParam<string>;
+
+        beforeEach(() => {
+            queryParam = new QueryParam<string>({
+                param: 'q',
+                serialize: DEFAULT_STRING_SERIALIZER,
+                deserialize: DEFAULT_STRING_DESERIALIZER,
+            });
+        });
+
+        it('updates the value immediately', () => {
+            queryParam.setValue('Test');
+            expect(queryParam.value).toBe('Test');
+        });
+
+        it('forwards the value change to registered listeners', () => {
+            const spy = jasmine.createSpy('fn');
+            queryParam._registerOnChange(spy);
+
+            queryParam.setValue('Test');
+            expect(spy).toHaveBeenCalledWith('Test');
+        });
+
+        it('does not forward the value change if instructed not to', () => {
+            const spy = jasmine.createSpy('fn');
+            queryParam._registerOnChange(spy);
+
+            queryParam.setValue('Test',  { emitEvent: false });
+            expect(spy).not.toHaveBeenCalled();
+        });
+    });
+
+    describe('updateValue', () => {
+        let queryParam: QueryParam<string>;
+
+        beforeEach(() => {
+            queryParam = new QueryParam<string>({
+                param: 'q',
+                serialize: DEFAULT_STRING_SERIALIZER,
+                deserialize: DEFAULT_STRING_DESERIALIZER,
+            });
+        });
+
+        it('emits the current value', fakeAsync(() => {
+            queryParam.value = 'Test';
+            queryParam.valueChanges.pipe(take(1)).subscribe({
+                next: value => expect(value).toBe('Test'),
+                error: () => fail('Expected emission'),
+            });
+
+            queryParam._updateValue();
+        }));
+
+        it('does not emit if instructed not to', fakeAsync(() => {
+            queryParam.value = 'Test';
+            queryParam.valueChanges.pipe(take(1)).subscribe({
+                next: value => fail(`Expected no emission, but received ${value}`),
+                error: () => fail('Expected no error'),
+            });
+
+            queryParam._updateValue({ emitEvent: false });
+        }));
+
+        it('notifies the parent', fakeAsync(() => {
+            const parent = {
+                _updateValue: jasmine.createSpy('fn'),
+            };
+
+            queryParam._setParent(parent as any);
+            queryParam.value = 'Test';
+            queryParam._updateValue();
+
+            expect(parent._updateValue).toHaveBeenCalled();
+        }));
+
+        it('does not notify the parent if instructed not to', fakeAsync(() => {
+            const parent = {
+                _updateValue: jasmine.createSpy('fn'),
+            };
+
+            queryParam._setParent(parent as any);
+            queryParam.value = 'Test';
+            queryParam._updateValue({ onlySelf: true });
+
+            expect(parent._updateValue).not.toHaveBeenCalled();
+        }));
     });
 });
