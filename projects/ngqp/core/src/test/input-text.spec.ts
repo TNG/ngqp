d0db52b1a9465232a723d4105542f6f9c0cd503c
Ingo Bürk <ingo.buerk@tngtech.com>
2019-01-12 17:44:58 +0100

    chore(tests): Added a first integration test

---
+++ projects/ngqp/core/src/test/input-text.spec.ts
@@ -0,0 +1,88 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { QueryParamBuilder, QueryParamGroup, QueryParamGroupDirective, QueryParamModule } from '../public_api';
+import { setupNavigationWarnStub } from './util';
+
+@Component({
+    template: `
+        <div [queryParamGroup]="paramGroup">
+            <input type="text" queryParamName="param" />
+        </div>
+    `,
+})
+class BasicTestComponent {
+
+    public paramGroup: QueryParamGroup;
+
+    constructor(private qpb: QueryParamBuilder) {
+        this.paramGroup = qpb.group({
+            param: qpb.stringParam({ param: 'q' }),
+        });
+    }
+
+}
+
+describe(QueryParamGroupDirective.name, () => {
+    let fixture: ComponentFixture<BasicTestComponent>;
+    let component: BasicTestComponent;
+    let input: HTMLInputElement;
+    let router: Router;
+
+    beforeEach(() => setupNavigationWarnStub());
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule.withRoutes([]),
+                QueryParamModule,
+            ],
+            declarations: [
+                BasicTestComponent,
+            ],
+        });
+
+        router = TestBed.get(Router);
+        TestBed.compileComponents();
+        router.initialNavigation();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(BasicTestComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+
+        input = (fixture.nativeElement as HTMLElement).querySelector('input') as HTMLInputElement;
+        fixture.detectChanges();
+    });
+
+    it('synchronizes a form control value change to the URL', fakeAsync(() => {
+        input.value = 'Test';
+        input.dispatchEvent(new Event('input'));
+        tick();
+
+        expect(router.url).toBe('/?q=Test');
+    }));
+
+    it('synchronizes a URL change to the form control', fakeAsync(() => {
+        router.navigateByUrl('/?q=Test');
+        tick();
+
+        expect(input.value).toBe('Test');
+    }));
+
+    it('synchronizes a programmatic change of the param to the URL', fakeAsync(() => {
+        component.paramGroup.get('param').setValue('Test');
+        tick();
+
+        expect(router.url).toBe('/?q=Test');
+    }));
+
+    it('synchronizes a programmatic change of the group to the URL', fakeAsync(() => {
+        component.paramGroup.setValue({ param: 'Test' });
+        tick();
+
+        expect(router.url).toBe('/?q=Test');
+    }));
+});
---
+++ projects/ngqp/core/src/test/util.ts
@@ -0,0 +1,13 @@
+/** See https://github.com/angular/angular/issues/25837 */
+export function setupNavigationWarnStub() {
+    const warn = console.warn;
+
+    spyOn(console, 'warn').and.callFake((...args: any[]) => {
+        const [firstArg] = args;
+        if (typeof firstArg === 'string' && firstArg.startsWith('Navigation triggered outside Angular zone')) {
+            return;
+        }
+
+        return warn.apply(console, args);
+    });
+}
