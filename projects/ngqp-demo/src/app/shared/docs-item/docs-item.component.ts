3d1a7930ab9a91fd8dfccbeeb60d22de987faaf9
Ingo Bürk <ingo.buerk@tngtech.com>
2019-01-13 16:04:35 +0100

    fix(docs): Don't track unnecessary event

--- projects/ngqp-demo/src/app/shared/docs-item/docs-item.component.ts
+++ projects/ngqp-demo/src/app/shared/docs-item/docs-item.component.ts
@@ -4,7 +4,6 @@ import { FragmentsService } from '../docs-fragment/fragments.service';
 import { ActivatedRoute } from '@angular/router';
 import { getPageForRoute } from '../../demo-docs.routes';
 import { DocsPage } from '../../docs-page';
-import { AnalyticsService } from '../analytics.service';
 
 @Component({
     selector: 'docs-item',
@@ -20,13 +19,8 @@ export class DocsItemComponent {
     public navigationCollapsed = true;
 
     constructor(public fragmentService: FragmentsService,
-                private analytics: AnalyticsService,
                 private route: ActivatedRoute) {
         this.docsPage = getPageForRoute(this.route.snapshot.url[0]);
-    }
-
-    public onTocClick(name: string): void {
-        this.analytics.trackEvent(`Clicked TOC Link: ${this.docsPage}:${name}`);
     }
 
 }
