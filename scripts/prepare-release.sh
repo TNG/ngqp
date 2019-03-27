fb960e01a15fb09f00771ef3eac9a21b5c2fbd6b
Ingo Bürk <ingo.buerk@tngtech.com>
2019-03-27 16:24:05 +0100

    chore: remove --sign from standard-version
    This has been removed for now since it doesn't seem to work with
    commitlint
    
    Signed-off-by: Ingo Bürk <ingo.buerk@tngtech.com>

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -10,7 +10,7 @@ echo "[2] Removing dist folder"
 rm -rf dist/
 
 echo "[3] Updating CHANGELOG"
-cd projects/ngqp/core; standard-version --sign --infile ../../../CHANGELOG.md; cd -
+cd projects/ngqp/core; standard-version --infile ../../../CHANGELOG.md; cd -
 
 echo "[4] Building @ngqp/core"
 yarn run core:build
