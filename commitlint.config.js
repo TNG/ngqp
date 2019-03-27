d58b487bc47748b7026ff137cf9ca8ca40b016bd
Ingo Bürk <ingo.buerk@tngtech.com>
2019-03-27 16:21:29 +0100

    chore: add release scope to commitlint
    Signed-off-by: Ingo Bürk <ingo.buerk@tngtech.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
     extends: [ '@commitlint/config-conventional' ],
     rules: {
         'subject-case': [ 0 ],
-        'scope-enum': [ 2, 'always', [ 'core', 'demo', 'docs', 'scripts', 'other', 'deps' ] ],
+        'scope-enum': [ 2, 'always', [ 'core', 'demo', 'docs', 'scripts', 'other', 'deps', 'release' ] ],
         'type-enum': [ 2, 'always', [ 'feat', 'fix', 'chore', 'test', 'docs', 'style', 'refactor', 'revert' ] ],
         'signed-off-by': [ 2, 'always', 'Signed-off-by:' ],
     },
