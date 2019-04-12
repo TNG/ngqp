428e266f950f15dea079dfd786755da15f91b940
Ingo Bürk <ingo.buerk@tngtech.com>
2019-04-12 13:45:05 +0200

    feat(core): support asynchronous deserializers
    We now support deserializers which run asynchronously by returning an
    observable instead. This is useful when the deserializer relies on some
    asynchronous process or data which has to be fetched first.
    
    relates #93
    
    Signed-off-by: Ingo Bürk <ingo.buerk@tngtech.com>

--- projects/ngqp/core/src/lib/model/query-param.ts
+++ projects/ngqp/core/src/lib/model/query-param.ts
@@ -1,5 +1,6 @@
-import { Observable, Subject } from 'rxjs';
-import { areEqualUsing, isFunction, isMissing, isPresent, undefinedToNull, wrapTryCatch } from '../util';
+import { forkJoin, isObservable, Observable, of, Subject } from 'rxjs';
+import { first } from 'rxjs/operators';
+import { areEqualUsing, isFunction, isMissing, isPresent, undefinedToNull, wrapIntoObservable, wrapTryCatch } from '../util';
 import { Comparator, OnChangeFunction, ParamCombinator, ParamDeserializer, ParamSerializer, Partitioner, Reducer } from '../types';
 import { QueryParamGroup } from './query-param-group';
 import { MultiQueryParamOpts, PartitionedQueryParamOpts, QueryParamOpts, QueryParamOptsBase } from './query-param-opts';
@@ -117,12 +118,6 @@ export abstract class AbstractQueryParam<U, T> extends AbstractQueryParamBase<T>
         this.combineWith = combineWith;
     }
 
-    /** @internal */
-    public abstract serializeValue(value: T | null): (string | null) | (string | null)[];
-
-    /** @internal */
-    public abstract deserializeValue(value: (string | null) | (string | null)[]): T | null;
-
     /**
      * Updates the value of this parameter.
      *
@@ -180,12 +175,12 @@ export class QueryParam<T> extends AbstractQueryParam<T | null, T | null> implem
     }
 
     /** @internal */
-    public deserializeValue(value: string | null): T | null {
+    public deserializeValue(value: string | null): Observable<T | null> {
         if (this.emptyOn !== undefined && value === null) {
-            return this.emptyOn;
+            return of(this.emptyOn);
         }
 
-        return this.deserialize(value);
+        return wrapIntoObservable(this.deserialize(value)).pipe(first());
     }
 
 }
@@ -212,12 +207,14 @@ export class MultiQueryParam<T> extends AbstractQueryParam<T | null, (T | null)[
     }
 
     /** @internal */
-    public deserializeValue(value: (string | null)[] | null): (T | null)[] | null {
-        if (this.emptyOn !== undefined && (value || []).length === 0) {
-            return this.emptyOn;
+    public deserializeValue(values: (string | null)[] | null): Observable<(T | null)[] | null> {
+        if (this.emptyOn !== undefined && (values || []).length === 0) {
+            return of(this.emptyOn);
         }
 
-        return (value || []).map(this.deserialize.bind(this));
+        return forkJoin<T | null>(...(values || [])
+            .map(value => wrapIntoObservable(this.deserialize(value)).pipe(first()))
+        );
     }
 
 }
--- projects/ngqp/core/src/test/serialize-deserialize.spec.ts
+++ projects/ngqp/core/src/test/serialize-deserialize.spec.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 import { QueryParamBuilder, QueryParamGroup, QueryParamModule } from '../public_api';
 import { setupNavigationWarnStub } from './util';
 
@@ -21,6 +23,28 @@ class BasicTestComponent {
             param: qpb.stringParam('q', {
                 serialize: value => value ? value.toUpperCase() : null,
                 deserialize: value => value ? value.toLowerCase() : null,
+            }),
+        });
+    }
+
+}
+
+@Component({
+    template: `
+        <div [queryParamGroup]="paramGroup">
+            <input type="text" queryParamName="param" />
+        </div>
+    `,
+})
+class AsyncTestComponent {
+
+    public paramGroup: QueryParamGroup;
+
+    constructor(private qpb: QueryParamBuilder) {
+        this.paramGroup = qpb.group({
+            param: qpb.stringParam('q', {
+                serialize: value => value ? value.toUpperCase() : null,
+                deserialize: value => of(value ? value.toLowerCase() : null).pipe(delay(500)),
             }),
         });
     }
@@ -72,6 +96,50 @@ describe('(de-)serialize', () => {
         router.navigateByUrl('/?q=TEST');
         tick();
 
+        expect(input.value).toBe('test');
+    }));
+});
+
+describe('asynchronous (de-)serialize', () => {
+    let fixture: ComponentFixture<AsyncTestComponent>;
+    let component: AsyncTestComponent;
+    let input: HTMLInputElement;
+    let router: Router;
+
+    beforeEach(() => setupNavigationWarnStub());
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule.withRoutes([]),
+                QueryParamModule,
+            ],
+            declarations: [
+                AsyncTestComponent,
+            ],
+        });
+
+        router = TestBed.get(Router);
+        TestBed.compileComponents();
+        router.initialNavigation();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AsyncTestComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+
+        input = (fixture.nativeElement as HTMLElement).querySelector('input') as HTMLInputElement;
+        fixture.detectChanges();
+    });
+
+    it('applies the async deserializer when the URL changes', fakeAsync(() => {
+        router.navigateByUrl('/?q=TEST');
+
+        tick();
+        expect(input.value).toBe('');
+
+        tick(500);
         expect(input.value).toBe('test');
     }));
 });
