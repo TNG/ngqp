472d0e1dcb50bd04a51c3c57dc194a19438a7b97
Ingo Bürk <ingo.buerk@tngtech.com>
2020-01-19 09:20:42 +0100

    chore: do not require commit signoff

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,5 @@ module.exports = {
         'subject-case': [ 0 ],
         'scope-enum': [ 2, 'always', [ 'core', 'demo', 'docs', 'scripts', 'other', 'deps', 'release' ] ],
         'type-enum': [ 2, 'always', [ 'feat', 'fix', 'chore', 'test', 'docs', 'style', 'refactor', 'revert' ] ],
-        'signed-off-by': [ 2, 'always', 'Signed-off-by:' ],
     },
 };
